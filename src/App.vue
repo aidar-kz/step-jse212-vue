01b0a391a2421f59db99cb2d1950bcd32c99d7b3
Aydeke <aydeke2@gmail.com>
2022-09-21 20:36:28 +0600

    Add BaseButton

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import BaseButton from '@/components/elements/BaseButton.vue';
+</script>
+
+<template>
+  <BaseButton>
+    Моя кнопка
+  </BaseButton>
+</template>
---
+++ src/components/elements/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="btn">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss">
+.btn {
+  min-width: 120px;
+  min-height: 40px;
+  padding: 0 14px;
+  font-size: 14px;
+  font-weight: 600;
+  color: $grey;
+}
+</style>
