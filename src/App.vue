94265474d6b4072fc19838f8db822988f1d80643
Aydeke <aydeke2@gmail.com>
2022-09-21 22:08:23 +0600

    Add BaseButton

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script setup>
+import { ref } from 'vue'
 import BaseButton from '@/components/elements/BaseButton.vue';
+import BaseInput from './components/elements/BaseInput.vue';
+
+const data = ref({
+  name: '',
+  email: '',
+  password: ''
+})
+
+const onClick = () => alert('Кнопка нажата!');
 </script>
 
 <template>
-  <BaseButton>
+  <BaseButton primary big @click="onClick">
     Моя кнопка
   </BaseButton>
+
+  <br>
+  <br>
+
+  <BaseInput type="text" label="Имя:" v-model="data.name" />
+  <BaseInput type="email" label="Эл.почта:" v-model="data.email" />
+  <BaseInput type="password" label="Пароль:" v-model="data.password" />
 </template>
+
+<style>
+#app {
+  width: 300px;
+  margin: 0 auto;
+}
+</style>
--- src/components/elements/BaseButton.vue
+++ src/components/elements/BaseButton.vue
@@ -1,5 +1,19 @@
+<script setup>
+defineProps({
+  primary: Boolean,
+  secondary: Boolean,
+  big: Boolean,
+  small: Boolean
+})
+</script>
+
 <template>
-  <button class="btn">
+  <button type="button" class="btn" :class="[
+    { primary },
+    { secondary },
+    { big },
+    { small },
+  ]">
     <slot />
   </button>
 </template>
@@ -12,5 +26,46 @@
   font-size: 14px;
   font-weight: 600;
   color: $grey;
+  background: transparent;
+  border-radius: 6px;
+  border: 1px solid #0000001F;
+  box-shadow: 0px 1px 4px #0000000d;
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    cursor: pointer;
+    color: $primary;
+    border-color: $primary;
+  }
+}
+
+.primary {
+  color: $white;
+  background: $primary;
+
+  &:hover {
+    color: $white;
+    background: lighten($primary, 10);
+  }
+}
+
+.secondary {
+  color: $white;
+  background: $secondary;
+
+  &:hover {
+    color: $white;
+    background: lighten($secondary, 10);
+  }
+}
+
+.big {
+  min-width: 140px;
+  min-height: 50px;
+}
+
+.small {
+  min-width: 80px;
+  min-height: 30px;
 }
 </style>
